f4732a5289c5c56ba18fe4206ae6f673b9709b41
Igor Avdeev <elephant@lislon.ru>
2015-10-20 22:06:41 +0300

    Disable many plugins to make vim faster

--- install/linux/install_vim.sh
+++ install/linux/install_vim.sh
@@ -2,16 +2,26 @@
 
 SRC_FILES=(~/dotfiles/vim/vimrc ~/dotfiles/vim/dotvim/*)
 
+mkdir -p ~/.vim
+
 for src_file in ${SRC_FILES[*]}; do
     if [[ $src_file =~ "dotvim" ]] ; then
-        dst_file="~/.vim/`basename $src_file`"
+        dst_file="$HOME/.vim/`basename $src_file`"
     else
-        dst_file="~/.`basename $src_file`"
+        dst_file="$HOME/.`basename $src_file`"
     fi
     if [[ -f "$TARGET" && ! -L $TARGET ]] ; then
         mv $TARGET "${TARGET}.bak"
         echo "Backup src_file done $TARGET -> ${TARGET}.bak"
     fi
+    ln -sf $src_file $dst_file
 done
+
+if [[ ! -d ~/.vimtmp/undo ]]; then
+    mkdir -p ~/.vimtmp/undo
+fi
+if [[ ! -d ~/.vimtmp/swp ]]; then
+    mkdir -p ~/.vimtmp/swp
+fi
 
 echo "Installation done"
