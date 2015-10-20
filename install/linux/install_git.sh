243e64c845f63db8e7405137781a09a71201e4b8
Igor Avdeev <elephant@lislon.ru>
2015-10-20 22:17:18 +0300

    Installation script for git

---
+++ install/linux/install_git.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+SOURCE=~/dotfiles/git/gitconfig
+TARGET=~/.gitconfig
+
+if [[ -f $TARGET && ! -L $TARGET ]] ; then
+    mv $TARGET "${TARGET}.bak"
+    echo "Backup file done $TARGET -> ${TARGET}.bak"
+fi
+
+ln -sf $SOURCE $TARGET
+echo "Installation done ($TARGET)"
