df5e3f72ee2c0386e973a9c83bce8f18ed9e2542
Igor Avdeev <elephant@lislon.ru>
2016-02-17 15:45:27 +0300

    Make zsh work with tramp

--- install/linux/install_zsh.sh
+++ install/linux/install_zsh.sh
@@ -3,6 +3,11 @@
 SOURCE=~/dotfiles/zsh/zshrc
 TARGET=~/.zshrc
 
+if ! which curl >/dev/null 2>&1; then
+    echo "Please install curl to use $0"
+    exit 1
+fi
+
 [[ -d ~/.oh-my-zsh ]] || sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 if [[ -f $TARGET && ! -L $TARGET ]] ; then
