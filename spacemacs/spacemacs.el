9a227c92b11a3e79d4bb1d73eee8bfa155078924
Igor Avdeev <elephant@lislon.ru>
2016-04-10 21:43:24 +0300

    Google translate interactive mode fix

--- spacemacs/spacemacs.el
+++ spacemacs/spacemacs.el
@@ -414,6 +414,7 @@ layers configuration."
      ("cyrillic-jcuken" . ("ru")))
    google-translate-translation-directions-alist
    '(("en" . "ru") ("ru" . "en") )
+   google-translate-pop-up-buffer-set-focus t
 
    ;; big undo
    undo-limit 200000                    ; 200kb per file
@@ -681,7 +682,7 @@ That function deletes \"#+ALLPRIORITIES\" line from index.org file"
 
     (use-local-map (make-sparse-keymap))
     (local-set-key (kbd "RET") 'my/translate-word-and-next-line)
-    (define-key evil-normal-state-map (kbd "RET") 'my/translate-word-and-next-line))
+    (evil-local-set-key 'normal (kbd "RET") 'my/translate-word-and-next-line))
 
   (defun my/google-translate-repl ()
     (interactive)
